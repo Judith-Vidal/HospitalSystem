697bac5bc574d974cc5bd37403ff7f1f89e955a1
Judith <jmoral58@students.solano.edu>
2024-11-05 22:55:16 -0800

    Es una prueba para ver si funciona.

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace HospitalSystem
 {
@@ -14,22 +16,44 @@ namespace HospitalSystem
     {
         public Login()
         {
-            InitializeComponent();
+            InitializeComponent(); // This method initializes the components of the login
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            // This method is called when the text in textBox1 changes
         }
 
         private void txtUsername_TextChanged(object sender, EventArgs e)
         {
-
+            // This methos is called when the text in the username field changes
         }
 
         private void txtPassword_TextChanged(object sender, EventArgs e)
         {
+            // This method is called when the text in the password field changes
+        }
 
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            // This methos is executed when the user clicks the login button
+            if (txtUsername.Text == "admin" && txtPassword.Text == "1234")
+            {
+                Main mainform = new Main(); // Create ans show the main window
+                mainform.Show();
+                this.Hide(); // Hide the login window
+            }
+            else
+            {
+                // If the username or password is incorrenct, show an error message
+                MessageBox.Show("Invalid username or password.\nTry again");
+            }
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // Clear the text fields for username and password
+            txtUsername.Text = string.Empty;
+            txtPassword.Text = string.Empty;
         }
     }
 }
